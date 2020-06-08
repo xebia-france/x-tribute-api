ff535bbbac0dfe81a6d240acd2b1b7c8b9c0e5ff
Benjamin Lacroix <blacroix@xebia.fr>
2020-06-08 11:49:16 +0200

    🔐 firebase admin secret

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
---output $HOME/firebase-admin-service-account.json $HOME/firebase-admin-service-account.json.gpg
+--output firebase-admin-service-account.json firebase-admin-service-account.json.gpg
